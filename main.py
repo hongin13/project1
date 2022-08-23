30bc7f277b613c7bdcffca850f4723923de1901a
hongin13 <dlsghd1213@naver.com>
2022-08-23 12:15:53 +0900

    sixth commit

--- main.py
+++ main.py
@@ -1,57 +1,44 @@
-# import cv2
-# import os
-# import numpy as np
-# import pandas as pd
-# import json
-# import tensorflow as tf
-# from tensorflow import keras
+#%%
+import os
+import numpy as np
+import pandas as pd
+import json
+import tensorflow as tf
+from tensorflow import keras
 
-# # labels = ['Rat', 'Cow', 'Tiger', 'Rabbit', 'Dragon', 'Snake',
-# #  'Horse', 'Sheep', 'Monkey', 'Chicken', 'Dog', 'Pig']
+labels = ['Rat', 'Cow', 'Tiger', 'Rabbit', 'Dragon', 'Snake',
+ 'Horse', 'Sheep', 'Monkey', 'Chicken', 'Dog', 'Pig']
 
-# l_json = []
-# for root, dirs, filenames in os.walk('json'):
-#     for filename in filenames:
-#         path = os.path.join(root, filename)
-#         with open(path, 'r') as f:
-#             json_data = json.load(f)
-#             l_json.append(json_data)
-#         # print(json.dumps(json_data))
+l_json = []
+for root, dirs, filenames in os.walk('json2'):
+    for filename in filenames:
+        path = os.path.join(root, filename)
+        with open(path, 'r') as f:
+            json_data = json.load(f)
+            l_json.append(json_data)
+        # print(json.dumps(json_data))
 
-# data = pd.DataFrame(l_json)
-# # print(df)
-
+#%%
+data = pd.DataFrame(l_json)
+print(data)
+print(data.shape)
 # print(data.isnull().sum() / len(data))
 
-# X = data.drop(['label'], axis=1)
-# y = data['label']
+#%%
+X = data.drop(['label'], axis=1)
+y = data['label']
+print(X.shape, y.shape)
 
-# from sklearn.model_selection import train_test_split
-# x_data, tt_x, y_data, tt_y = train_test_split(X, y, random_state=42, test_size=0.1)
+from sklearn.model_selection import train_test_split
+x_data, tt_x, y_data, tt_y = train_test_split(X, y, random_state=42, test_size=0.1)
+print(x_data.shape, tt_x.shape, y_data.shape, tt_y.shape)
 
-# from sklearn.tree import DecisionTreeClassifier
-# dt1 = DecisionTreeClassifier()
-# dt1.fit(x_data, y_data)
-# print(dt1.score(x_data, y_data), dt1.score(tt_x, tt_y))
-# print(dt1.predict(tt_x[:5]))
-# print(list(tt_y[:5]))
+#%%
+from sklearn.tree import DecisionTreeClassifier
+dt1 = DecisionTreeClassifier()
+dt1.fit(x_data, y_data)
+print(dt1.score(x_data, y_data), dt1.score(tt_x, tt_y))
+print(dt1.predict(tt_x[:5]))
+print(list(tt_y[:5]))
 
-# model = keras.layers.Sequential([
-#     keras.layers.Dense()
-# ])
-
-# ll_json = []
-# for root, dirs, filenames in os.walk('test_json'):
-#     for filename in filenames:
-#         path = os.path.join(root, filename)
-#         with open(path, 'r') as f:
-#             json_test_data = json.load(f)
-#             ll_json.append(json_test_data)
-
-# val_data = pd.DataFrame(ll_json)
-# print(dt1.predict(val_data))
-
-# # import matplotlib.pyplot as plt
-# # from sklearn.tree import plot_tree
-# # plot_tree(dt1)
-# # plt.show()
+# %%
--- save_data.py
+++ save_data.py
@@ -1,119 +1,118 @@
-import dlib
-import cv2
-import os
-import math
-import numpy as np
-import json
+# import dlib
+# import cv2
+# import os
+# import math
+# import numpy as np
+# import json
 
-meta = []
-labels = ['Rat', 'Cow', 'Tiger', 'Rabbit', 'Dragon', 'Snake',
-          'Horse', 'Sheep', 'Monkey', 'Chicken' ,'Dog', 'Pig']
+# meta = []
+# labels = ['Rat', 'Cow', 'Tiger', 'Rabbit', 'Dragon', 'Snake',
+#           'Horse', 'Sheep', 'Monkey', 'Chicken', 'Dog', 'Pig']
 
-for root, dirs, filenames in os.walk('Images'):
+# # for root, dirs, filenames in os.walk('Images'):
 # for root, dirs, filenames in os.walk('C:/Users/aischool/Desktop/image'):
-    for filename in filenames:
-        path = os.path.join(root, filename)
-        image = cv2.imread(path)
-        if image is None:
-            print("Error : ", path)
-            continue
-        cv2.imwrite(path, image)
-        label = -1
-        for index, name in enumerate(labels):
-            if name in root:
-                label = index
-                break
-        if label == -1:
-            continue
-        first, last = os.path.splitext(filename)
-        path = os.path.join(root, first + '.jpg')
-        meta.append((path, label))
+#     for filename in filenames:
+#         path = os.path.join(root, filename)
+#         image = cv2.imread(path)
+#         if image is None:
+#             print("Error : ", path)
+#             continue
+#         cv2.imwrite(path, image)
+#         label = -1
+#         for index, name in enumerate(labels):
+#             if name in root:
+#                 label = index
+#                 break
+#         if label == -1:
+#             continue
+#         first, last = os.path.splitext(filename)
+#         path = os.path.join(root, first + '.jpg')
+#         meta.append((path, label))
 
-# print(meta)
+# # print(meta)
 
-count = 0
-features = ['face', 'eyebrow', 'eyebtween', 'low', 'philtrum', 'uplib', 'downlib', 'nosewidth', 'noseheight', 'eyesize', 'mouth']
+# count = 0
+# features = ['face', 'eyebrow', 'eyebtween', 'low', 'philtrum', 'uplib', 'downlib', 'nosewidth', 'noseheight', 'eyesize', 'mouth']
 
-def distance(x1, y1, x2, y2):
-    result = math.sqrt(math.pow(x1-x2, 2) + math.pow(y1-y2, 2))
-    return result
+# def distance(x1, y1, x2, y2):
+#     result = math.sqrt(math.pow(x1-x2, 2) + math.pow(y1-y2, 2))
+#     return result
 
-def area(p1, p2, p3, p4):
-    d1 = distance(p1[0], p1[1], p2[0], p2[1])
-    d2 = distance(p3[0], p3[1], p4[0], p4[1])
-    return d1 / d2
+# def area(p1, p2, p3, p4):
+#     d1 = distance(p1[0], p1[1], p2[0], p2[1])
+#     d2 = distance(p3[0], p3[1], p4[0], p4[1])
+#     return d1 / d2
 
-dic_f = {
-    'l_face' : [0, 16, 19, 24, 8],
-    'l_eyebrow' : [21, 22, 0, 16],
-    'l_eyeb' : [39, 42, 0, 16],
-    'l_low' : [57, 8, 19, 24, 8],
-    'l_philtrum' : [33, 51, 19, 24, 8],
-    'l_uplib' : [51, 62, 19, 24, 8],
-    'l_downlib' : [66, 57, 19, 24, 8],
-    'l_nosew' : [31, 35, 0, 16],
-    'l_noseh' : [27, 33, 19, 24, 8],
-    'l_lefteye' : [36, 39, 0, 16],
-    'l_righteye' : [42, 45, 0, 16],
-    'l_mouth' : [48, 54, 0, 16]
-}
+# dic_f = {
+#     'l_face' : [0, 16, 19, 24, 8],
+#     'l_eyebrow' : [21, 22, 0, 16],
+#     'l_eyeb' : [39, 42, 0, 16],
+#     'l_low' : [57, 8, 19, 24, 8],
+#     'l_philtrum' : [33, 51, 19, 24, 8],
+#     'l_uplib' : [51, 62, 19, 24, 8],
+#     'l_downlib' : [66, 57, 19, 24, 8],
+#     'l_nosew' : [31, 35, 0, 16],
+#     'l_noseh' : [27, 33, 19, 24, 8],
+#     'l_lefteye' : [36, 39, 0, 16],
+#     'l_righteye' : [42, 45, 0, 16],
+#     'l_mouth' : [48, 54, 0, 16]
+# }
 
-dic = {}
-print(len(meta))
-print(meta)
-n = 0
-meta_size = len(meta) * 0.1
+# dic = {}
+# print(len(meta))
+# # print(meta)
+# n = 0
 
+# for i, l in meta:
+#     print(n)
+#     n += 1
+#     # print(i, l)
+#     result = []
+#     count += 1
+#     print(i)
 
+#     image = cv2.imread(i)
+#     print(image.shape)    
+#     detector = dlib.get_frontal_face_detector()
+#     predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+#     print(1)
+#     rects = detector(image[..., ::-1], 1)
+#     if not len(rects):
+#         continue
+#     dst = image.copy()
+#     print(2)
+#     for rect in rects:
+#         shape = predictor(image[..., ::-1], rect)
 
-for i, l in meta:
-    print(n)
-    n += 1
-    # print(i, l)
-    result = []
-    count += 1
-    print(i)
-
-    image = cv2.imread(i)
-    print(image.shape)    
-    detector = dlib.get_frontal_face_detector()
-    predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
-
-    rects = detector(image[..., ::-1], 1)
-    print(rects)
-    dst = image.copy()
-
-    for rect in rects:
-        shape = predictor(image[..., ::-1], rect)
-
-    eyebrow = []
-    for k, v in dic_f.items():
-        for i in v:
-            part = shape.part(i)
-            p = (part.x, part.y)
-            eyebrow.append(p)
-    # print(eyebrow)
-        if len(eyebrow) == 5:
-            a = eyebrow[2]
-            b = eyebrow[3]
-            c = eyebrow.pop()
-            eyebrow[2] = ((a[0] + b[0]) / 2, (a[1] + b[1]) / 2)
-            eyebrow[3] = c
-        # print(eyebrow)
-        val = area(eyebrow[0], eyebrow[1], eyebrow[2], eyebrow[3])
-        result.append((val, l))
-
-    le = result[9]
-    re = result[10]
-    m = result.pop()
-    result[9] = ((le[0] + re[0]) / 2, int((le[1] + re[1]) / 2))
-    result[10] = m
-    for i in range(len(result)):
-        dic['label'] = labels[result[i][1]]
-        dic[features[i]] = result[i][0]
+#         eyebrow = []
+#         for k, v in dic_f.items():
+#             for i in v:
+#                 part = shape.part(i)
+#                 p = (part.x, part.y)
+#                 eyebrow.append(p)
+#     # print(eyebrow)
+#             if len(eyebrow) == 5:
+#                 a = eyebrow[2]
+#                 b = eyebrow[3]
+#                 c = eyebrow.pop()
+#                 eyebrow[2] = ((a[0] + b[0]) / 2, (a[1] + b[1]) / 2)
+#                 eyebrow[3] = c
+#             # print(eyebrow)
+#             val = area(eyebrow[0], eyebrow[1], eyebrow[2], eyebrow[3])
+#             result.append((val, l))
+#             print(result)
     
-    print(dic)
-    for items in dic:
-        # with open(f'json/features_{count}.json', 'w') as f:
-        with open(f'json2/features_{count}.json', 'w') as f:
-            json.dump(dic, f)
+#     le = result[9]
+#     re = result[10]
+#     m = result.pop()
+#     result[9] = ((le[0] + re[0]) / 2, int((le[1] + re[1]) / 2))
+#     result[10] = m
+#     for i in range(len(result)):
+#         dic['label'] = labels[result[i][1]]
+#         dic[features[i]] = result[i][0]
+    
+#     print(dic)
+#     for items in dic:
+#         # with open(f'json/features_{count}.json', 'w') as f:
+#         with open(f'json2/features_{count}.json', 'w') as f:
+#             json.dump(dic, f)
